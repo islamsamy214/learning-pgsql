b1c4a620847797ce6d396cd0c4d8492ab0b806bd
islamsamy214 <islamsamy214@gmail.com>
2024-11-01 12:00:35 +0400

    Refactor subqueries in subqueries.sql

--- Subqueries/subqueries.sql
+++ Subqueries/subqueries.sql
@@ -2,7 +2,13 @@ SELECT name, area
 FROM cities
 WHERE name IN (
         SELECT name FROM cities
-    )
+    ) -- when its in where condition the return has to be a single column
 
-SELECT name, (SELECT COUNT(*) FROM cities)
+SELECT name, (SELECT COUNT(*) FROM cities) -- when it is in select statement the return has to be a single value, one column and one row
 FROM cities
+
+SELECT name, area
+FROM (
+    SELECT name, area FROM cities -- when its in from clause it can be a table
+)
+WHERE area > 20;
